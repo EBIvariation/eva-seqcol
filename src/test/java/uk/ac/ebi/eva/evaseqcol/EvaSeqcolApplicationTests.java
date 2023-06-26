09179a8c385a6f5ab1410479ee84e3a64c4c48bd
waterflow80 <thessalonikaathena@outlook.com>
2023-06-26 14:35:49 +0100

    Created the spring boot project with the necessary dependencies
    - Added the necessary dependencies
    - Added a maven profile 'seqcol' that has credentials needed in src/main/resources/application.properties
    that will be provided at compilation time. (the maven profile is added in pom.xml)

---
+++ src/test/java/uk/ac/ebi/eva/evaseqcol/EvaSeqcolApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.eva.evaseqcol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaSeqcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
