1ac485277c70fb25a668a7e2c0378021cfe87e2a
waterflow80 <thessalonikaathena@outlook.com>
2023-06-28 01:32:48 +0100

    Implemented the process of downloading the assembly sequences fasta file
        Created and tested the classes for the following:
        1. Downloand the assembly sequences' fasta file from NCBI database
        2. Unzip the _fna.gz file
        3. Parse the fasta file, retrieve the sequences and hash them with md5 algorithm
        4. Save the sequences into the AssemblySequencesEntity class

--- src/main/java/uk/ac/ebi/eva/evaseqcol/datasource/AssemblySequencesDataSource.java
+++ src/main/java/uk/ac/ebi/eva/evaseqcol/datasource/AssemblySequencesDataSource.java
@@ -1,12 +1,12 @@
 package uk.ac.ebi.eva.evaseqcol.datasource;
 
-import uk.ac.ebi.eva.evaseqcol.entities.AssemblySequenceEntity;
+import uk.ac.ebi.eva.evaseqcol.entities.AssemblySequencesEntity;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 public interface AssemblySequencesDataSource {
-    Optional<AssemblySequenceEntity> getAssemblySequencesByAccession(String accession)
+    Optional<AssemblySequencesEntity> getAssemblySequencesByAccession(String accession)
             throws IOException, NoSuchAlgorithmException;
 }
