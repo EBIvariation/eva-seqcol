0a835387ee780d38c596e184c698fb96524de570
waterflow80 <thessalonikaathena@outlook.com>
2023-07-07 10:33:01 +0100

    fixed the sha512 checksum calculator

--- src/main/java/uk/ac/ebi/eva/evaseqcol/refget/SHA512Calculator.java
+++ src/main/java/uk/ac/ebi/eva/evaseqcol/refget/SHA512Calculator.java
@@ -1,7 +1,5 @@
 package uk.ac.ebi.eva.evaseqcol.refget;
 
-import javax.xml.bind.DatatypeConverter;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -15,18 +13,15 @@ public class SHA512Calculator extends ChecksumCalculator{
      * */
     public String calculateChecksum(String sequence) {
         String sequence1 = sequence.replaceAll("[\\W]|_", ""); // Remove non-alphanumeric characters
-        String sequence2 =  SHA512Hash(sequence1.toUpperCase());
-        byte[] sequenceBytes = sequence2.getBytes();
+        byte[] digest = SHA512Hash(sequence1.toUpperCase());
 
         // Encode the first 24 bytes with Base64
-        byte[] truncatedSequence = Arrays.copyOfRange(sequenceBytes, 0, 24);
+        byte[] truncatedSequence = Arrays.copyOfRange(digest, 0, 24);
         String encodedBase64Sequence = Base64.getUrlEncoder().encodeToString(truncatedSequence);
-
-        String finalSequence = "SQ." + encodedBase64Sequence;
-        return finalSequence;
+        return "SQ." + encodedBase64Sequence;
     }
 
-    public String SHA512Hash(String text) {
+    public byte[] SHA512Hash(String text) {
         MessageDigest md;
         try {
             md = MessageDigest.getInstance("SHA-512");
@@ -34,9 +29,6 @@ public class SHA512Calculator extends ChecksumCalculator{
             throw new RuntimeException(e);
         }
         md.update(text.getBytes());
-        byte[] digest = md.digest();
-        String textHash = DatatypeConverter
-                .printHexBinary(digest).toUpperCase();
-        return textHash.toLowerCase();
+        return md.digest();
     }
 }
