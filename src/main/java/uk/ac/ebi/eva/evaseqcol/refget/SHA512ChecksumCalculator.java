75061f2bbc9ea97dac5a25456d25d953a8ebbe5a
waterflow80 <thessalonikaathena@outlook.com>
2023-08-01 18:27:11 +0100

    method enhancement

--- src/main/java/uk/ac/ebi/eva/evaseqcol/refget/SHA512ChecksumCalculator.java
+++ src/main/java/uk/ac/ebi/eva/evaseqcol/refget/SHA512ChecksumCalculator.java
@@ -13,13 +13,7 @@ public class SHA512ChecksumCalculator {
      * Used to calculate the 'sequence's digest.
      * */
     public String calculateRefgetChecksum(String sequence) {
-        String sequence1 = sequence.replaceAll("[\\W]|_", ""); // Remove non-alphanumeric characters
-        byte[] digest = SHA512Hash(sequence1.toUpperCase());
-
-        // Encode the first 24 bytes with Base64
-        byte[] truncatedSequence = Arrays.copyOfRange(digest, 0, 24);
-        String encodedBase64Sequence = Base64.getUrlEncoder().encodeToString(truncatedSequence);
-        return "SQ." + encodedBase64Sequence;
+        return "SQ." + calculateChecksum(sequence);
     }
 
     /**
