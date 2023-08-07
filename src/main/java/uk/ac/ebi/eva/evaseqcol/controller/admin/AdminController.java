3abdb308770ad7d15b0d473c2aacbc66430000e1
waterflow80 <thessalonikaathena@outlook.com>
2023-08-07 15:00:40 +0100

    cahnged method name

--- src/main/java/uk/ac/ebi/eva/evaseqcol/controller/admin/AdminController.java
+++ src/main/java/uk/ac/ebi/eva/evaseqcol/controller/admin/AdminController.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import uk.ac.ebi.eva.evaseqcol.entities.SeqColEntity;
+import uk.ac.ebi.eva.evaseqcol.exception.AssemblyNotFoundException;
 import uk.ac.ebi.eva.evaseqcol.exception.DuplicateSeqColException;
 import uk.ac.ebi.eva.evaseqcol.service.SeqColService;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/collection/admin")
 @RestController
@@ -31,7 +30,7 @@ public class AdminController {
      * Fetch and insert all possible seqCol objects given the assembly accession
      * NOTE: All possible means with all naming conventions that exist in the fetched assembly report*/
     @PutMapping(value = "/seqcols/{asmAccession}")
-    public ResponseEntity<?> fetchAndInsertSeqColByAssemblyAccessionAndNamingConvention(
+    public ResponseEntity<?> fetchAndInsertSeqColByAssemblyAccession(
             @PathVariable String asmAccession) {
         try {
             List<String> level0Digests = seqColService.fetchAndInsertAllSeqColByAssemblyAccession(asmAccession);
@@ -45,6 +44,8 @@ public class AdminController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (DuplicateSeqColException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+        } catch (AssemblyNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
