2ab467bca20fcbc6117260ebebafae2ca5fd21ab
waterflow80 <thessalonikaathena@outlook.com>
2023-08-24 11:33:12 +0100

    snapshot 1 - dependencies springdoc and springfox

--- src/main/java/uk/ac/ebi/eva/evaseqcol/controller/admin/AdminController.java
+++ src/main/java/uk/ac/ebi/eva/evaseqcol/controller/admin/AdminController.java
@@ -1,5 +1,9 @@
 package uk.ac.ebi.eva.evaseqcol.controller.admin;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.ebi.eva.evaseqcol.exception.AssemblyNotFoundException;
 import uk.ac.ebi.eva.evaseqcol.exception.DuplicateSeqColException;
+import uk.ac.ebi.eva.evaseqcol.exception.IncorrectAccessionException;
 import uk.ac.ebi.eva.evaseqcol.service.SeqColService;
 
 import java.io.IOException;
@@ -29,21 +34,39 @@ public class AdminController {
     /**
      * Fetch and insert all possible seqCol objects given the assembly accession
      * NOTE: All possible means with all naming conventions that exist in the fetched assembly report*/
+    @Operation(summary = "Add new sequence collection objects",
+            description = "Given an INSDC or RefSeq accession, this endpoint will fetch the corresponding assembly " +
+                    "report and the assembly sequences FASTA file from the NCBI datasource, use them to construct " +
+                    "seqCol objects with as many naming conventions as possible (depends on the naming conventions " +
+                    "contained in the assembly report) and eventually save these seqCol objects into the database. " +
+                    "This is an authenticated endpoint, so it requires admin privileges to run it.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "seqCol object(s) successfully inserted"),
+            @ApiResponse(responseCode = "409", description = "seqCol object(s) already exist(s)"),
+            @ApiResponse(responseCode = "404", description = "Assembly not found"),
+            @ApiResponse(responseCode = "400", description = "Bad request. (It can be a bad accession value)"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @PutMapping(value = "/seqcols/{asmAccession}")
     public ResponseEntity<?> fetchAndInsertSeqColByAssemblyAccession(
-            @PathVariable String asmAccession) {
+            @Parameter(name = "asmAccession",
+            description = "INSDC or RefSeq assembly accession",
+            example = "GCA_000146045.2",
+            required = true) @PathVariable String asmAccession) {
         try {
             List<String> level0Digests = seqColService.fetchAndInsertAllSeqColByAssemblyAccession(asmAccession);
             return new ResponseEntity<>(
                     "Successfully inserted seqCol object(s) for assembly accession " + asmAccession + "\nSeqCol digests=" + level0Digests
                     , HttpStatus.OK);
+        } catch (IncorrectAccessionException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         catch (IOException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (DuplicateSeqColException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
         } catch (AssemblyNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
