8bdfb4810622cac78391256d4def49d60efd3ba0
tcezard <tcezard@ebi.ac.uk>
2026-07-03 22:46:28 +0100

    start postgresql container once

--- src/test/java/uk/ac/ebi/eva/evaseqcol/utils/AbstractIntegrationTest.java
+++ src/test/java/uk/ac/ebi/eva/evaseqcol/utils/AbstractIntegrationTest.java
@@ -1,19 +1,17 @@
 package uk.ac.ebi.eva.evaseqcol.utils;
 
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class AbstractIntegrationTest {
     private static final String POSTGRES_IMAGE = "postgres:11";
 
-    @Container
-    @ServiceConnection
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(POSTGRES_IMAGE);
+    public static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(POSTGRES_IMAGE);
+
+    static {
+        postgreSQLContainer.start();
+    }
 
     @DynamicPropertySource
     static void dataSourceProperties(DynamicPropertyRegistry registry) {
